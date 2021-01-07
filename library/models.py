7247fd3f04f12da0e08eca5d15572bf09f172592
Bocian <michalxbocian@gmail.com>
2021-01-07 16:25:51 +0100

    Book and Opinions models

--- library/models.py
+++ library/models.py
@@ -1,3 +1,18 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from django.db.models import CharField, IntegerField, ForeignKey, CASCADE
+
+
+class Book(models.Model):
+    isnb = CharField(max_length=13)
+    title = CharField(max_length=40)
+    author = CharField(max_length=40)
+    genre = CharField(max_length=40)
+
+
+class Opinion(models.Model):
+    isnb = CharField(max_length=13)
+    mark = IntegerField(default=0)
+    description = CharField(max_length=50)
+
+    book = ForeignKey(Book, on_delete=CASCADE)

eb9bb2e2d944192ad0ab6cb7c953c04395f62c8a
Bocian <michalxbocian@gmail.com>
2021-01-21 19:46:21 +0100

    func view with decor api_view

--- library/views.py
+++ library/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from library.models import Book
+from library.serializers import BookSerializer
+
+
+@api_view(['GET', 'POST'])
+def book_list(request, format=None):
+    if request.method == 'GET':
+        snippets = Book.objects.all()
+        serializer = BookSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def book_detail(request, pk, fomrat=None):
+    try:
+        book = Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'GET':
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        book.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
